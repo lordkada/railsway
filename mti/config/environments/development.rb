549090fe6c191a24cbd9a23ae88bd5cc68898534
Carlo Alberto Degli Atti <lordkada@gmail.com>
2011-08-26 10:26:52 -0700

    dev sync

--- mti/config/environments/development.rb
+++ mti/config/environments/development.rb
@@ -1,4 +1,4 @@
-TellTheShop::Application.configure do
+RailsWay::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
